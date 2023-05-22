67f6d695b259db1a617af49bdc436486e8f32557
CarolinaH <carolinaheredialozano@gmail.com>
2023-05-22 18:33:47 -0500

    Added the main file where I will be working on the movie recommendation system. It holds the basic guidelines set for the code to work.

---
+++ movie_recommendation_sys.py
@@ -0,0 +1,32 @@
+# 1. Create a class called Movie with the following attributes: title, genre, rating, and year.
+class movie:
+    def __init__(self, title, genre, rating, year):
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+        self.year = year
+
+    # 2. Implement methods in the Movie class to set and get the attributes.
+    def get_movie(self):
+        
+
+# 3. Use a dictionary to store movie information, where the movie title is the key, 
+# and the Movie object is the value.
+
+# 4. Implement a function to add movies to the system. The function should take movie 
+# information as input and create a Movie object, storing it in the movie dictionary.
+
+# 5. Implement a function to recommend movies to a user based on their preferred genre. The
+# function should take a genre as input and return a list of movies with the highest ratings 
+# in that genre.
+
+# 6. Use the NumPy package to calculate the average rating for movies in a given genre.
+
+# 7. Implement a function to export the movie dictionary to a JSON file.
+
+# 8. Implement a function to import movie information from a JSON file and update the movie 
+# dictionary accordingly.
+
+# 9. Write tests for all the implemented functions to ensure their correctness.
+
+# 10. Use the math package to round the average ratings to two decimal places.
