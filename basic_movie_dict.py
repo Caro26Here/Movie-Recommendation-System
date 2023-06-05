ca54afe30c22a6539babee66ff8e38023edd2d4a
CarolinaH <carolinaheredialozano@gmail.com>
2023-06-05 09:56:34 -0500

    Finished the second most rudimentary working movie recommendation system. It's missing limiting characteristics for input data (making sure its valid data entries) as well as importing an established working directory instead of adding each movie to the directory.

--- basic_movie_dict.py
+++ basic_movie_dict.py
@@ -1,49 +1,49 @@
 def box_office(genre):
     match genre:
         case 1: # drama
-            movie = ['Killers of the Flower Moon','Drama',2023,0]
+            movie = ['Killers of the Flower Moon', 'Drama', 0, 2023]
         case 2: # comedy
-            movie = ['Barbie','Comedy',2023,0]
+            movie = ['Barbie', 'Comedy', 0, 2023]
         case 3: # action
-            movie = ['Fast X','Action',2023,6.3]
+            movie = ['Fast X', 'Action', 6.3, 2023]
         case 4: # romance
-            movie = ['The Little Mermaid','Romance',2023,7.0]
+            movie = ['The Little Mermaid', 'Romance', 7.0, 2023]
         case 5: # sci-fy
-            movie = ['Ant-Man and the Wasp: Quantumania','Sci-fy',2023,6.2]
+            movie = ['Ant-Man and the Wasp: Quantumania', 'Sci-fy', 6.2, 2023]
         case 6: # horror
-            movie = ['Evil Dead Rise','Horror',2023,6.7]
+            movie = ['Evil Dead Rise', 'Horror', 6.7, 2023]
         case 7: # fantasy
-            movie = ['The Super Mario Bros. Movie','Fantasy',2023,7.2]
+            movie = ['The Super Mario Bros. Movie', 'Fantasy', 7.2, 2023]
         case 8: # thriller
-            movie = ['John Wick: Chapter 4','Thriller',2023,8]
+            movie = ['John Wick: Chapter 4', 'Thriller', 8, 2023]
         case 9: # animation
-            movie = ['Spider-Man: Across the Spider-Verse','Animation',2023,0]
+            movie = ['Spider-Man: Across the Spider-Verse', 'Animation', 0, 2023]
         case 10: # adventure
-            movie = ['Guardians of the Galaxy Vol. 3','Adventure',2023,8.2]
+            movie = ['Guardians of the Galaxy Vol. 3', 'Adventure', 8.2, 2023]
 
     return movie
 
 def top_250(genre):
     match genre:
         case 1: # drama
-            movie = ['The Shawshank Redemption','Drama',1994,9.3]
+            movie = ['The Shawshank Redemption', 'Drama', 9.3, 1994]
         case 2: # comedy
-            movie = ['Modern Times','Comedy',1936,8.5]
+            movie = ['Modern Times', 'Comedy', 8.5, 1936]
         case 3: # action
-            movie = ['The Dark Knight','Action',2008,9.0]
+            movie = ['The Dark Knight', 'Action', 9.0, 2008]
         case 4: # romance
-            movie = ['Before Sunrise','Romance',1995,8.1]
+            movie = ['Before Sunrise', 'Romance', 8.1, 1995]
         case 5: # sci-fy
-            movie = ['Interstelar','Sci-fy',2014,8.7]
+            movie = ['Interstelar', 'Sci-fy', 8.7, 2014]
         case 6: # horror
-            movie = ['The shining','Horror',1980,8.4]
+            movie = ['The shining', 'Horror', 8.4, 1980]
         case 7: # fantasy
-            movie = ['Spirited Away','Fantasy',2001,8.6]
+            movie = ['Spirited Away', 'Fantasy', 8.6, 2001]
         case 8: # thriller
-            movie = ['Se7en','Thriller',1995,8.6]
+            movie = ['Se7en', 'Thriller', 8.6, 1995]
         case 9: # animation
-            movie = ['The Lion King','Animation',1994,8.5]
+            movie = ['The Lion King', 'Animation', 8.5, 1994]
         case 10: # adventure
-            movie = ['The Lord of the Rings: The Return of the King','Adventure',2003,9.0]
+            movie = ['The Lord of the Rings: The Return of the King', 'Adventure', 9.0, 2003]
 
     return movie
--- basic_movie_recommendation_system.py
+++ basic_movie_recommendation_system.py
@@ -1,51 +1,70 @@
-from imdb import Cinemagoer
 import numpy as np
-
-ia = Cinemagoer()
 
 # 1. Create a class called Movie with the following attributes: title, genre, rating, and year.
 class movie:
-    def __init__(self, title, genre, rating, year):
+    def __init__(self, title, genre, rating, year): # read about class constructor (python).
         self.title = title
         self.genre = genre
         self.rating = rating
         self.year = year
 
     # 2. Implement methods in the Movie class to set and get the attributes.
-    def get_movie(self, genre):
-        top = ia.get_top50_movies_by_genres(genre)
-        top[0]
-        print('Title recommended:',top[0])
+    # This initially becomes redundant when declaring them in the constructor method of the class.
+    # It is recommended when further logic and complex programming takes place.
+    # def set_movie(self):
+    #     pass
     
-    
+    # This initially becomes redundant when declaring them in the constructor method of the class.
+    # It is recommended when further logic and complex programming takes place.
+    # def get_movie(self):
+    #     pass
 
-movie1 = movie('Inception','Science Fiction',8.8,2010)
-print('Movie:',movie1.title)
-print('Genre:',movie1.genre)
-
-# 3. Use a dictionary to store movie information, where the movie title is the key, 
-# and the Movie object is the value.
-new_movie = movie(input('Título de la película: '),input('Genero: '),input('Rating: '),input('Año de estreno: '))
-movies_dict = {(movie.title): new_movie}
+# To check dictionary structure:        
+# movie1 = movie('Inception', 'Science Fiction', 8.8, 2010)
+# print('Movie:', movie1.title)
+# print('Genre:', movie1.genre)
+# print('')
 
 # 4. Implement a function to add movies to the system. The function should take movie 
 # information as input and create a Movie object, storing it in the movie dictionary.
 def add_movie(movies_dict):
-    new_movie =  movie(input('Ingresa una peli: '),input('Ingresa el genero de la peli: '),input('Ingresa el rating de la peli: '),input('Ingresa el año de estreno de la peli: '))
-    movies_dict.append(new_movie)
+    new_movie = movie(input('Title: '), input('Genre: '), input('Rating: '), input('Year: '))
+    movies_dict[new_movie.title] = new_movie
+    print('')
+    #movies_dict.append(new_movie)
 
-add_movie()
+# 3. Use a dictionary to store movie information, where the movie title is the key, 
+# and the Movie object is the value.
+count = 0
+movies_dict = {}
+while count < 3:
+    #new_movie = movie(input('Title: '), input('Genre: '), input('Rating: '), input('Year: '))
+    #movies_dict = {(new_movie.title): new_movie}
+    add_movie(movies_dict)
+    count += 1
+
+# Check the current working directory by printing it:
+# for title, new_movie in movies_dict.items():
+#     print('Title:' + new_movie.title)
+#     print('Genre:' + new_movie.genre)
+#     print('')
 
 # 5. Implement a function to recommend movies to a user based on their preferred genre. The
 # function should take a genre as input and return a list of movies with the highest ratings 
 # in that genre.
-movie_input = input('Ingresa el título de una película que te encante: ')
+def movie_recommend(genre_input, movies_dict):
+    genre_input
+    
+    for title, movie in movies_dict.items():
+        if (genre_input == movie.genre):
+            print('____________________________')
+            print('RECOMMENDED MOVIE:')
+            print('Title:' + title)
+            print('Rating:' + movie.rating)
+            print('____________________________')
 
-def movie_recommend(movie_input,movie_dict):
-    movie_input
-
-    movie = movie()
-    movie.get_movie('action')
+genre_input = input('Desired movie genre: ')
+movie_recommend(genre_input, movies_dict)
 
 # 6. Use the NumPy package to calculate the average rating for movies in a given genre.
 
